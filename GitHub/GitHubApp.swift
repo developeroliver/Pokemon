5c94558a9ffe8fc45f2f2e12d4a7d3f1e9d2f466
developeroliver <developpeur.oliver@gmail.com>
2025-03-22 18:25:30 +0100

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by olivier geiger on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
